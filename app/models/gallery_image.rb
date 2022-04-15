0e0170d3bf85b64704ce9727b5893ad2b0fafeba
Vaibhav Dwivedi <vaibhav111dwivedi@gmail.com>
2022-04-15 15:45:32 +0530

    Deleted models

--- app/models/gallery_image.rb
+++
@@ -1,6 +0,0 @@
-class GalleryImage < ApplicationRecord
-  mount_uploader :image, GalleryImageUploader
-
-  validates :title, presence: true
-  validates :image, presence: true
-end
--- app/models/gallery_video.rb
+++
@@ -1,6 +0,0 @@
-class GalleryVideo < ApplicationRecord
-  mount_uploader :video, GalleryVideoUploader
-
-  validates :title, presence: true
-  validates :video, presence: true
-end
