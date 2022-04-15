35b215848b6954399185c9421215f1b801f9370d
Vaibhav Dwivedi <vaibhav111dwivedi@gmail.com>
2022-04-15 13:59:29 +0530

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,26 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def gallery
+  	
+  end
+  def about_us
+  	
+  end
+  def mission
+  	
+  end
+  def chairman_message
+  	
+  end
+  def principal_message
+  	
+  end
+  def mandatory_public_disclosure
+  end
+
+  def contact
+    
+  end
+end
