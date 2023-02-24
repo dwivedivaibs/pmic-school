23f37270dd559c3c69159ff38c62365d4224a3ae
Vaibhav D <vaibhav111dwivedi@gmail.com>
2023-02-24 16:21:02 +0530

    Update school name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '3.93.189.160', port: 22, roles: [:web, :app, :db], primary: true
+server '54.196.138.203', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,'git@github.com:dwivedivaibs/pmic-school.git'
 set :application,     'pmic-school'
